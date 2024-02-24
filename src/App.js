56f13ca508f3599d808cc9c212337a0103181378
rmiyoyo <miyoyoraphael@gmail.com>
2024-02-24 17:01:26 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Greeting from './Greeting';
+import Greeting from './routes/Greeting';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Greeting />} />
+        <Route exact path="/" element={<Greeting />} />
       </Routes>
     </Router>
   );
